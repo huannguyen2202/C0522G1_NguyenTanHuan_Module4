30cd7d824af5259249a07aace9beb504d6eaf3aa
huannguyen2202 <huannguyenbk97@gmail.com>
2022-09-21 19:58:50 +0700

    kkkkk

---
+++ case_study/src/main/java/case_study/dto/CustomerDto.java
@@ -0,0 +1,2 @@
+package case_study.dto;public class CustomerDto {
+}
---
+++ ss07_spring_data_repository/exercise/blog_app/src/main/java/com/example_blog/repository/IBlogsRepository.java
@@ -0,0 +1,28 @@
+package com.example_blog.repository;
+
+
+import com.example_blog.dto.IBlogDto;
+import com.example_blog.model.Blogs;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface IBlogsRepository extends JpaRepository<Blogs,Integer> {
+
+    Blogs findById(int id);
+
+    @Query(value = "select * from blogs where name_blog like %:keyword% ",nativeQuery = true)
+    Page<Blogs> searchByName(@Param("keyword") String name, Pageable pageable);
+
+    @Query(value = "select  * from blogs order by date_created DESC ",nativeQuery = true)
+    Page<Blogs> findAll(Pageable pageable);
+    @Query(value = "select b.name_blog as nameBlog, c.name_category as nameCategory from blogs b join category c on b.category_id=c.id",nativeQuery = true )
+    List<IBlogDto> showList();
+}
